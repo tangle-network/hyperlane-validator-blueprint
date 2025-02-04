c25d0a9808de3a80bfadc9dcc92f2bac17709e07
Serial <69764315+Serial-ATA@users.noreply.github.com>
2025-02-04 13:13:28 -0500

    chore(ci): make test pass in ci

--- build.rs
+++ build.rs
@@ -5,4 +5,8 @@ fn main() {
     build::utils::soldeer_install();
     build::utils::soldeer_update();
     build::utils::build_contracts(contract_dirs);
+
+    println!("cargo:rerun-if-changed=src/lib.rs");
+    println!("cargo:rerun-if-changed=src/main.rs");
+    build::blueprint_metadata::generate_json();
 }
